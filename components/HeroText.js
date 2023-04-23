e7ca3a1b4aab3ba4128bde05ae66a64e72ad87c5
Mohamed Riumaan <riuman.21@icloud.com>
2023-04-24 00:19:19 +0500

    fdsifdFS

--- components/HeroText.js
+++ components/HeroText.js
@@ -33,7 +33,7 @@ const HeroText = () => {
             animate = {{opacity : 1 }}
             transition = {{duration : .8 }}
             className="text text-[#1D1D1F] font-bold lg:text-[9rem] text-[3.5rem] whitespace-nowrap lg:leading-[105px] leading-[50px] z-10 opacity-100">Curious<br></br>developer,<br></br>code breaker,<br></br>storyteller</motion.p>
-            <Star className="absolute top-0 lg:left-[-2rem] left-[-2rem] -z-10 group-hover:rotate-180 transition-all duration-700 ease-out" />
+            <Star className="absolute lg:top-0 top-[-2rem] lg:left-[-2rem] left-[-1rem] -z-10 group-hover:rotate-180 transition-all duration-700 ease-out" />
             <Heart className="absolute bottom-0 lg:left-[-3rem] left-[-1rem] -z-10 group-hover:rotate-[-34deg] transition-all duration-300 ease-out" />
             <Tag className="absolute lg:right-[10rem] right-[.5rem] top-5 -z-10 group-hover:rotate-12 transition-all duration-300 ease-out" />
         </div>
