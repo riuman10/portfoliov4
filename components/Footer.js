a3002a1ed812c1738c9f120f93bfd865ce6aad9a
Mohamed Riumaan <riuman.21@icloud.com>
2023-04-29 23:00:39 +0500

    animation remove

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,14 @@
 import HeartFooter from "./icons/HeartFooter";
 import { motion } from "framer-motion";
 
-
 const Footer = () => {
-    return (
-        <motion.div 
-        initial = {{opacity : 0 , y :10}}
-        whileInView={{opacity :1 , y : 0}}
-        viewport={{once : false, amount :.7}}
-        transition = {{duration : .6 , ease : "easeIn"}}
-        className="text-center py-1 flex items-center text-lg bg-[#1D1D1F] justify-center bg-transparent">
-            <p className="flex items-center font-medium text-white">Think different  </p>
-        </motion.div>
-    )
-}
-
+  return (
+    <div className="text-center py-1 flex items-center text-lg bg-[#1D1D1F] justify-center bg-transparent">
+      <p className="flex items-center font-medium text-white">
+        Think different {" "}
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
