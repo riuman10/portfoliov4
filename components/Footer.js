ae8f23482171a194215e169e2533e92edaab78e5
Mohamed Riumaan <riuman.21@icloud.com>
2023-04-29 22:58:55 +0500

    footer changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,17 @@
 import HeartFooter from "./icons/HeartFooter";
-
+import { motion } from "framer-motion";
 
 
 const Footer = () => {
     return (
-        <div className="text-center py-2 flex items-center justify-center bg-transparent border-t border-gray-50">
-            <p className="flex items-center font-light">Made with <HeartFooter /> from Maldives</p>
-        </div>
+        <motion.div 
+        initial = {{opacity : 0 , y :10}}
+        whileInView={{opacity :1 , y : 0}}
+        viewport={{once : false, amount :.7}}
+        transition = {{duration : .6 , ease : "easeIn"}}
+        className="text-center py-1 flex items-center text-lg bg-[#1D1D1F] justify-center bg-transparent">
+            <p className="flex items-center font-medium text-white">Think different  </p>
+        </motion.div>
     )
 }
 
