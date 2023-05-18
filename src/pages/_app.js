b4d1b2cb3307c353a6387c2dba47e39e198993ef
Mohamed Riumaan <riuman.21@icloud.com>
2023-05-18 14:03:39 +0500

    Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
 import Layout from 'components/Layout'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
