77aefd342f585f7e82093b01ce8ac8e2c33efd49
Mohamed Riumaan <riuman.21@icloud.com>
2023-08-13 13:59:14 +0500

    color changes

--- components/Card.js
+++ components/Card.js
@@ -45,7 +45,7 @@ const Card = ({
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.4 }}
+            transition={{ duration: 0.35 }}
             exit={{ opacity: 0, height: 0 }}
             Layout="inside"
             className="w-[300px]"
