f43cab0915bf1f4f9a4eda4fc290733d7393388a
riumaan10 <riuman.21@icloud.com>
2023-09-03 15:22:15 +0500

    testing screens

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import HeroText from "./HeroText";
 
 const Hero = () => {
     return (
-        <div className="h-screen flex items-center justify-center lg:mx-0 mx:8">
+        <div className="lg:h-screen h-[80vh] flex items-center justify-center lg:mx-0 mx:8">
             <HeroText />
         </div>
     )
